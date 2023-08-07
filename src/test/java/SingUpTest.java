e349a6a8fd85591303df10a0158657a229bedc57
kamilbeb <bebkampoczta@gmail.com>
2023-08-07 19:13:40 +0200

    Add sing up test with invalid email

--- src/test/java/SingUpTest.java
+++ src/test/java/SingUpTest.java
@@ -65,4 +65,30 @@ public class SingUpTest {
         softAssert.assertAll();
 
     }
+    @Test
+    public void singUpInvalidEmail() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream()
+                .filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+        driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+
+        String lastName = "Testowy";
+
+        String email = "tester.pl";
+        driver.findElement(By.name("firstname")).sendKeys("Kamil");
+        driver.findElement(By.name("lastname")).sendKeys(lastName);
+        driver.findElement(By.name("phone")).sendKeys("111111111");
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys("Test1234");
+        driver.findElement(By.name("confirmpassword")).sendKeys("Test1234");
+        driver.findElement(By.xpath("//button[@type = 'submit' and text()=' Sign Up']")).click();
+
+        WebElement errors = driver.findElement(By.xpath("//div[@class='resultsignup']//p"));
+        Assert.assertEquals(errors.getText(),"The Email field must contain a valid email address.");
+
+    }
 }
