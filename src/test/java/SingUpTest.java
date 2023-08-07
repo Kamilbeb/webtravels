035c2e9eab92095f447b30d01158ee290eb2a792
kamilbeb <bebkampoczta@gmail.com>
2023-08-07 18:19:06 +0200

    Add sign up test

--- src/test/java/SingUpTest.java
+++ src/test/java/SingUpTest.java
@@ -7,8 +7,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.List;
-import java.util.stream.Collectors;
+
 
 public class SingUpTest {
     @Test
@@ -21,6 +20,21 @@ public class SingUpTest {
         driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream()
                 .filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
         driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+
+        String lastName = "Testowy";
+        int randomNumber = (int) (Math.random()*1000);
+        String email = "nowytester"+randomNumber+"@tester.pl";
+        driver.findElement(By.name("firstname")).sendKeys("Kamil");
+        driver.findElement(By.name("lastname")).sendKeys(lastName);
+        driver.findElement(By.name("phone")).sendKeys("111111111");
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys("Test1234");
+        driver.findElement(By.name("confirmpassword")).sendKeys("Test1234");
+        driver.findElement(By.xpath("//button[@type = 'submit' and text()=' Sign Up']")).click();
+
+        WebElement heading = driver.findElement(By.xpath("//h3[@class='RTL']"));
+        Assert.assertTrue(heading.getText().contains(lastName));
+        Assert.assertEquals(heading.getText(),"Hi, Kamil Testowy");
 
     }
 }
