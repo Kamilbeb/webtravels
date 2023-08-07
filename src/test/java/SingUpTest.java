55cbdc91c645651f1eb19f8e7af794e234d3f528
kamilbeb <bebkampoczta@gmail.com>
2023-08-07 19:05:59 +0200

    Add sing up empty form test

--- src/test/java/SingUpTest.java
+++ src/test/java/SingUpTest.java
@@ -5,8 +5,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
+import java.util.List;
+
 
 
 public class SingUpTest {
@@ -35,6 +38,31 @@ public class SingUpTest {
         WebElement heading = driver.findElement(By.xpath("//h3[@class='RTL']"));
         Assert.assertTrue(heading.getText().contains(lastName));
         Assert.assertEquals(heading.getText(),"Hi, Kamil Testowy");
+
+    }
+
+    @Test
+    public void singUpEmptyForm() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream()
+                .filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+        driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+        driver.findElement(By.xpath("//button[@type = 'submit' and text()=' Sign Up']")).click();
+
+        List<String> error = driver.findElements(By.xpath("//div[@class='resultsignup']//p"))
+                .stream().map(WebElement::getText).toList();
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(error.contains("The Email field is required."));
+        softAssert.assertTrue(error.contains("The Password field is required."));
+        softAssert.assertTrue(error.contains("The Password field is required."));
+        softAssert.assertTrue(error.contains("The First name field is required."));
+        softAssert.assertTrue(error.contains("The Last Name field is required."));
+        softAssert.assertAll();
 
     }
 }
