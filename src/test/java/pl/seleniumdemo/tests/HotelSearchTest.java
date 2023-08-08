a1766ca09a136cb9d5b5ae1f753c440143590064
kamilbeb <bebkampoczta@gmail.com>
2023-08-08 11:10:10 +0200

    Add second page object Add list hotels

--- src/test/java/pl/seleniumdemo/tests/HotelSearchTest.java
+++ src/test/java/pl/seleniumdemo/tests/HotelSearchTest.java
@@ -1,9 +1,9 @@
 package pl.seleniumdemo.tests;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.seleniumdemo.pages.HotelSearchPage;
+import pl.seleniumdemo.pages.ResultsPage;
 
 import java.util.List;
 
@@ -17,9 +17,8 @@ public class HotelSearchTest extends BaseTest {
         hotelSearchPage.setTravellers();
         hotelSearchPage.performSearch();
 
-
-        List<String> hotelNames = driver.findElements(By.xpath("//h4[contains(@class, 'list_title')]//b"))
-                .stream().map(el -> el.getAttribute("textContent")).toList();
+        ResultsPage resultsPage = new ResultsPage(driver);
+        List<String> hotelNames = resultsPage.getHotelNames();
 
 
         Assert.assertEquals(hotelNames.get(0),"Jumeirah Beach Hotel");
